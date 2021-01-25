d44b58cb7d8922b6afed489d392df68f6e76f8d4
Colin Bradley <colin@colinbradley.co.uk>
2021-01-25 17:09:46 +0000

    Initial replace code

---
+++ src/main.rs
@@ -0,0 +1,74 @@
+use regex::{Match, Regex};
+use std::{
+    fs::{self, DirEntry},
+    io,
+    path::Path,
+};
+
+fn main() -> io::Result<()> {
+    let cell_delimiters = Regex::new(r#"(?mi)(?:(?:[a-z]+\d*)|")(\.)[a-z]+"#).unwrap();
+    let member_fetch = Regex::new(r#"(?mi)(?:(?:[a-z]\d*)|}|\))(:)[a-z]"#).unwrap();
+    let sourced_address = Regex::new(r#"(?mi)(?: |"|\(|\[)(!)[a-z]"#).unwrap();
+    let namespace = Regex::new(r#"(?mi)[a-z]\d*(#)[a-z]"#).unwrap();
+
+    visit_dirs(&Path::new("."), &|entry| {
+        let path = entry.path();
+
+        println!("Reading {:?}", path);
+
+        let mut content = fs::read_to_string(&path).unwrap();
+        let original = content.clone();
+
+        for captures in cell_delimiters.captures_iter(&original) {
+            let capture: Match = captures.get(1).unwrap();
+            content.replace_range(capture.start()..capture.end(), ":");
+        }
+
+        for captures in member_fetch.captures_iter(&original) {
+            let capture: Match = captures.get(1).unwrap();
+            content.replace_range(capture.start()..capture.end(), ".");
+        }
+
+        for captures in sourced_address.captures_iter(&original) {
+            let capture: Match = captures.get(1).unwrap();
+            content.replace_range(capture.start()..capture.end(), "~");
+        }
+
+        // Reverse because we're adding characters
+        for captures in namespace
+            .captures_iter(&original)
+            .collect::<Vec<_>>()
+            .iter()
+            .rev()
+        {
+            let capture: Match = captures.get(1).unwrap();
+            content.replace_range(capture.start()..capture.end(), "::");
+        }
+
+        println!("Old content:\n{}", original);
+        println!("New content:\n{}", content);
+
+        fs::write(String::from(path.to_str().unwrap()) + ".new", content).expect("Oh noes");
+    })?;
+
+    Ok(())
+}
+
+fn visit_dirs(dir: &Path, cb: &dyn Fn(&DirEntry)) -> io::Result<()> {
+    if dir.is_file() {
+        return Ok(());
+    }
+
+    for entry in fs::read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        let is_file = path.to_str().unwrap().ends_with(&".acs");
+        if path.is_dir() {
+            visit_dirs(&path, cb)?;
+        } else if is_file {
+            cb(&entry);
+        }
+    }
+
+    Ok(())
+}
